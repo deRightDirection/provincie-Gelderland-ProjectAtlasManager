d79cb13fa88538b7fc21e9a4d73f2cf2bac525eb
Mannus Etten <info@therightdirection.nl>
2022-11-24 10:05:22 +0100

    cards layout

--- src/app/itemsview/itemsview.component.ts
+++ src/app/itemsview/itemsview.component.ts
@@ -1,26 +1,7 @@
+import Portal from '@arcgis/core/portal/Portal';
+import PortalItem from "@arcgis/core/portal/PortalItem";
+import PortalQueryResult from "@arcgis/core/portal/PortalQueryResult";
 import { Component, OnInit, AfterViewInit, ViewEncapsulation, Inject } from '@angular/core';
-import { compile, detach } from '@syncfusion/ej2-base';
-import { cardBook } from './data-source';
-import { MultiSelect, SelectEventArgs, RemoveEventArgs } from '@syncfusion/ej2-dropdowns';
-import { Query, DataManager, Predicate } from '@syncfusion/ej2-data';
-/**
- * Default Card Component
- */
-// tslint:disable:max-line-length
-interface FilterKey {
-    Code: string;
-}
-let card: NodeList;
-let cardEle: HTMLElement;
-let cardObj: JSON[] = cardBook as JSON[];
-let data: Object[] = [];
-let multiSelectData: Object[] = [];
-let searchData: Object[] = [];
-let searchValCount: number = 0; let filterCategory: { [key: string]: Object; }[] = [{ Name: 'Client-Side', Code: 'client' }, { Name: 'Server-Side', Code: 'server' }, { Name: 'Front-End', Code: 'ui' }];
-let emptyData: boolean = true;
-
-/*  Initialize MultiSelect component */
-let multiselectComp: MultiSelect;
 
 @Component({
   selector: 'app-itemsview',
@@ -29,96 +10,44 @@ let multiselectComp: MultiSelect;
     encapsulation: ViewEncapsulation.None
 })
 export class ItemsviewComponent  implements OnInit {
-    public DataList: any = [];
+  private AGOLPortal : Portal = new Portal();
+  public DataList: any = [];
     public ngOnInit(): void {
-        this.DataList = cardBook;
+      this.AGOLPortal.authMode = 'immediate';
+      this.AGOLPortal.load().then(() => {
+        console.log(this.AGOLPortal.restUrl);
+        console.log("portal id: " + this.AGOLPortal.id);
+ //       var tagSearches = bouwBlokken.flatMap(bb => "(tags:\"" + bb + "," + x + "\" AND NOT tags:\"vervallen\")");
+        let queryParams = {
+          num: 150,
+
+//          query: "(" + tagSearches.join(" OR ") + ") orgid:" + this.AGOLPortal.id + " (type:(\"Feature Service\"))"
+          query: "orgid:" + this.AGOLPortal.id + " AND type:(\"Web Map\")"
+        };
+        console.log(queryParams);
+        this.AGOLPortal.queryItems(queryParams).then((result: PortalQueryResult) => {
+          var xx = this.getBouwblokNamen(result);
+        });
+      });
     }
     ngAfterViewInit(): void {
-        multiselectComp = new MultiSelect({
-            // set the local data to dataSource property
-            dataSource: filterCategory,
-            // map the appropriate columns to fields property
-            fields: { text: 'Name', value: 'Code' },
-            // set the placeholder to MultiSelect input element
-            placeholder: 'Search by categories',
-            select: this.multiSelectFun.bind(this),
-            removed: this.multiSelectRemove.bind(this),
-        });
-        multiselectComp.appendTo('#local');
-        this.cardRendering(cardObj);
-        document.getElementById('search_Card')!.onkeyup = (e: KeyboardEvent) => {
-            if (e.code === 'Tab' || e.code === 'Escape' || e.code === 'ShiftLeft' || (e.code === 'Backspace' && emptyData)) {
-                return;
-            }
-            let inputVal: string = (e.target as HTMLInputElement).value;
-            inputVal.length === 0 ? emptyData = true : emptyData = false;
-            this.searchFilter(inputVal);
-        };
-    }
-    /* Remove event function for multiSelect component */
-    public multiSelectRemove(e: RemoveEventArgs): void {
-        let cardDa: Object[] = searchData.length > 0 ? searchData : (multiSelectData.length > 0 ? multiSelectData : cardObj);
-        if (multiselectComp.value && multiselectComp.value.length === 0 && searchValCount === 0) {
-            multiSelectData = cardDa;
-            this.cardRendering(cardObj);
-        } else if (multiselectComp.value.length === 0 && searchValCount > 0) {
-            this.searchFilter((document.getElementById('search_Card') as HTMLInputElement).value);
-        } else if (multiselectComp.value.length === 0) {
-            multiSelectData = cardDa;
-            this.cardRendering(cardDa);
-        } else {
-            let keywords: string[] = (e.itemData as FilterKey).Code.split(',');
-            let dublicate: Object[];
-            keywords.forEach((key: string): void => {
-                dublicate = new DataManager(cardObj as JSON[]).executeLocal(new Query().where('cardImage.tag', 'Contains', key, true));
-                dublicate.forEach((da: Object): void => {
-                    if (cardDa.indexOf(da) !== -1) {
-                        cardDa.splice(cardDa.indexOf(da), 1);
-                    }
-                });
-                multiSelectData = cardDa;
-            });
-            this.cardRendering(multiSelectData);
-        }
     }
 
-    public multiSelectFun(e: SelectEventArgs): void {
-        let keywords: string[] = (e.itemData as FilterKey).Code.split(','); let dublicate: Object[];
-        let cardDa: Object[] = searchData.length > 0 ? searchData : cardObj;
-        if (multiselectComp.value && multiselectComp.value.length === 0 && searchValCount === 0) {
-            multiSelectData = [];
-        }
-        keywords.forEach((key: string): void => {
-            dublicate = new DataManager(cardDa as JSON[]).executeLocal(new Query().where('cardImage.tag', 'Contains', key, true));
-            if (dublicate.length === 0) {
-                multiSelectData = [];
-                return;
-            }
-            dublicate.forEach((da: Object): void => {
-                if (multiSelectData.indexOf(da) === -1) {
-                    multiSelectData.push(da);
-                }
-            });
-        });
-        this.cardRendering(multiSelectData);
-    }
-    /* Function for Filtering Cards */
-    public searchFilter(key: string): void {
-        searchValCount = key.length;
-        let predicate: Predicate = new Predicate('cardContent', 'Contains', key, true);
-        predicate = predicate.or('cardImage.title', 'Contains', key, true).or('header_title', 'Contains', key, true).or('header_subtitle', 'Contains', key, true);
-        let cardDa: Object[] = (multiSelectData.length > 0 && multiselectComp.value.length > 0) ? multiSelectData : cardObj;
-        searchData = data = new DataManager(cardDa as JSON[]).executeLocal(new Query().where(predicate));
-        this.cardRendering(data);
-    }
-    public cardRendering(cardObj: Object[]): void {
-        let errorContent: HTMLElement = document.querySelector('.tile_layout .row.error') as HTMLElement;
-        if (cardObj.length > 0) {
-            errorContent.style.display = 'none';
-            this.DataList = cardObj;
-        } else {
-            this.DataList = [];
-            errorContent.style.display = 'flex';
-        }
-    }
+    /** haal alle bouwblok namen op */
+  getBouwblokNamen(result: PortalQueryResult){
+    console.log(result.results.length);
+
+    var cards: Object[] = [];
+
+    result.results.forEach((item: PortalItem) => {
+      console.log(item.title);
+      console.log(item.thumbnailUrl);
+      var card = {
+        Title : item.title,
+        Thumbnail: item.thumbnailUrl
+      }
+      cards.push(card);
+    });
+    this.DataList = cards;
+  }
 }
